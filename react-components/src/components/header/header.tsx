d25a698a346880c88781d6e337634d3b2321420d
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-03-10 11:43:58 +0300

    feat: set routing and create header

---
+++ react-components/src/components/header/header.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './index.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="header__container">
+          <nav className="header__navigation">
+            <ul className="navigation">
+              <li className="navigation__item">
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li className="navigation__item">
+                <NavLink to="/about">About us</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ react-components/src/pages/main.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return <div>Home content</div>;
+  }
+}
+
+export default Home;
