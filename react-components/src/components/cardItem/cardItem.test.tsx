748a99d5f1b5b48c4fdf9200bc9437f11185977a
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-03-18 18:30:48 +0300

    fix: add mockData in cardItem test

--- react-components/src/components/cardItem/cardItem.test.tsx
+++ react-components/src/components/cardItem/cardItem.test.tsx
@@ -3,38 +3,24 @@ import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import CardItem from './cardItem';
 
+const mockData = {
+  id: 1,
+  title: 'Steel Analog Couple Watches',
+  price: 35,
+  rating: 4.79,
+  brand: 'Eastern Watches',
+  category: 'womens-watches',
+  thumbnail: 'https://i.dummyjson.com/data/products/66/thumbnail.jpg',
+};
+
 describe('Tests for CardItem component', () => {
   it('Has cardItem a category text', () => {
-    render(
-      <CardItem
-        product={{
-          id: 1,
-          title: 'Steel Analog Couple Watches',
-          price: 35,
-          rating: 4.79,
-          brand: 'Eastern Watches',
-          category: 'womens-watches',
-          thumbnail: 'https://i.dummyjson.com/data/products/66/thumbnail.jpg',
-        }}
-      />
-    );
+    render(<CardItem product={mockData} />);
     expect(screen.getByTestId('card-category')).toHaveTextContent('Category');
   });
 
   it('Is a header h3 of the cardItem not empty', () => {
-    render(
-      <CardItem
-        product={{
-          id: 1,
-          title: 'Steel Analog Couple Watches',
-          price: 35,
-          rating: 4.79,
-          brand: 'Eastern Watches',
-          category: 'womens-watches',
-          thumbnail: 'https://i.dummyjson.com/data/products/66/thumbnail.jpg',
-        }}
-      />
-    );
+    render(<CardItem product={mockData} />);
     expect(
       screen.getByRole('heading', {
         level: 3,
@@ -43,90 +29,30 @@ describe('Tests for CardItem component', () => {
   });
 
   it('Is cart-category not empty', () => {
-    render(
-      <CardItem
-        product={{
-          id: 1,
-          title: 'Steel Analog Couple Watches',
-          price: 35,
-          rating: 4.79,
-          brand: 'Eastern Watches',
-          category: 'womens-watches',
-          thumbnail: 'https://i.dummyjson.com/data/products/66/thumbnail.jpg',
-        }}
-      />
-    );
+    render(<CardItem product={mockData} />);
 
     expect(screen.getByTestId('card-category')).not.toBeEmptyDOMElement();
   });
 
   it('Has cardItem a brand text', () => {
-    render(
-      <CardItem
-        product={{
-          id: 1,
-          title: 'Steel Analog Couple Watches',
-          price: 35,
-          rating: 4.79,
-          brand: 'Eastern Watches',
-          category: 'womens-watches',
-          thumbnail: 'https://i.dummyjson.com/data/products/66/thumbnail.jpg',
-        }}
-      />
-    );
+    render(<CardItem product={mockData} />);
     expect(screen.getByTestId('card-brand')).toHaveTextContent('Brand');
   });
 
   it('Is cart-brand not empty', () => {
-    render(
-      <CardItem
-        product={{
-          id: 1,
-          title: 'Steel Analog Couple Watches',
-          price: 35,
-          rating: 4.79,
-          brand: 'Eastern Watches',
-          category: 'womens-watches',
-          thumbnail: 'https://i.dummyjson.com/data/products/66/thumbnail.jpg',
-        }}
-      />
-    );
+    render(<CardItem product={mockData} />);
 
     expect(screen.getByTestId('card-brand')).not.toBeEmptyDOMElement();
   });
 
   it('Has cardItem a image tag', () => {
-    render(
-      <CardItem
-        product={{
-          id: 1,
-          title: 'Steel Analog Couple Watches',
-          price: 35,
-          rating: 4.79,
-          brand: 'Eastern Watches',
-          category: 'womens-watches',
-          thumbnail: 'https://i.dummyjson.com/data/products/66/thumbnail.jpg',
-        }}
-      />
-    );
+    render(<CardItem product={mockData} />);
 
     expect(screen.getByRole('img')).toBeTruthy();
   });
 
   it('Is cart-raiting not empty', () => {
-    render(
-      <CardItem
-        product={{
-          id: 1,
-          title: 'Steel Analog Couple Watches',
-          price: 35,
-          rating: 4.79,
-          brand: 'Eastern Watches',
-          category: 'womens-watches',
-          thumbnail: 'https://i.dummyjson.com/data/products/66/thumbnail.jpg',
-        }}
-      />
-    );
+    render(<CardItem product={mockData} />);
 
     expect(screen.getByTestId('card-raiting')).not.toBeEmptyDOMElement();
   });
