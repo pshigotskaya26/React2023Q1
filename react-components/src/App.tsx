144939001bd1bae46a6f48ff108c2e18259bbae7
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-03-19 16:52:55 +0300

    fix: change functional components on class components

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Routes, BrowserRouter as Router, HashRouter } from 'react-router-dom';
-
-//import { useState } from 'react';
-//import reactLogo from './assets/react.svg';
 import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
@@ -10,27 +7,30 @@ import Home from './pages/main/main';
 import About from './pages/about/about';
 import NotFound from './pages/404';
 
-export function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main className="main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-
-      <Footer />
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export function WrappedApp() {
-  return (
-    <Router>
-      <App />
-    </Router>
-  );
+export class WrappedApp extends Component {
+  render() {
+    return (
+      <Router>
+        <App />
+      </Router>
+    );
+  }
 }
