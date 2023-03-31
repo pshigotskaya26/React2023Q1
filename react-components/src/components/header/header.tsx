1dbb18a66d2b5ec4101c001b189b821bc82048d3
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-03-31 15:32:25 +0300

    feat: rewrite class components Header and Footer on function component

--- react-components/src/components/header/header.tsx
+++ react-components/src/components/header/header.tsx
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './index.css';
 
-class Header extends Component {
-  render() {
-    const pathPage = window.location.pathname.slice(1);
+const Header = () => {
+  const pathPage = window.location.pathname.slice(1);
 
-    return (
-      <header className="header">
-        <div className="container header__container">
-          {pathPage === '' ? (
-            <h2 data-testid="header__title">Current page: Home</h2>
-          ) : pathPage === 'about' ? (
-            <h2 data-testid="header__title">Current page: About us</h2>
-          ) : pathPage === 'form' ? (
-            <h2>Current page: Form</h2>
-          ) : (
-            <h2 data-testid="header__title">Current page: Not Found</h2>
-          )}
-          <nav className="header__navigation">
-            <ul className="navigation">
-              <li className="navigation__item">
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li className="navigation__item">
-                <NavLink to="/about">About us</NavLink>
-              </li>
-              <li className="navigation__item">
-                <NavLink to="/form">Form</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
-  }
-}
+  return (
+    <header className="header">
+      <div className="container header__container">
+        {pathPage === '' ? (
+          <h2 data-testid="header__title">Current page: Home</h2>
+        ) : pathPage === 'about' ? (
+          <h2 data-testid="header__title">Current page: About us</h2>
+        ) : pathPage === 'form' ? (
+          <h2>Current page: Form</h2>
+        ) : (
+          <h2 data-testid="header__title">Current page: Not Found</h2>
+        )}
+        <nav className="header__navigation">
+          <ul className="navigation">
+            <li className="navigation__item">
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="navigation__item">
+              <NavLink to="/about">About us</NavLink>
+            </li>
+            <li className="navigation__item">
+              <NavLink to="/form">Form</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
