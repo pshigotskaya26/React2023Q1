8097ace45e62cb814c9c5546bc1f5c8f78ef7f4e
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-04-02 14:41:23 +0300

    feat: rewrite class components About and NotFound on functional components

--- react-components/src/pages/about/about.tsx
+++ react-components/src/pages/about/about.tsx
@@ -1,18 +1,16 @@
 import Header from '../../components/header/header';
-import React, { Component } from 'react';
+import React from 'react';
 import './index.css';
 
-class About extends Component {
-  render() {
-    return (
-      <section className="about">
-        <Header />
-        <div className="container">
-          <h1 className="about__title">About us</h1>
-        </div>
-      </section>
-    );
-  }
-}
+const About = () => {
+  return (
+    <section className="about">
+      <Header />
+      <div className="container">
+        <h1 className="about__title">About us</h1>
+      </div>
+    </section>
+  );
+};
 
 export default About;
