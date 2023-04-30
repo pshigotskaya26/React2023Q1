318f60b40ae1d25b62d67a284746905928f7f177
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-04-30 08:04:13 +0300

    fix: fix delete window.__INITIAL_STATE

--- react-components/src/entry-client.tsx
+++ react-components/src/entry-client.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { Provider } from 'react-redux';
@@ -8,13 +8,14 @@ import { RootState } from './store/store';
 
 declare global {
   interface Window {
-    __INITIAL_STATE__: RootState;
+    __INITIAL_STATE__?: RootState;
   }
 }
 
 const store = initStore(window.__INITIAL_STATE__);
+delete window.__INITIAL_STATE__;
 
-hydrateRoot(
+ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <Provider store={store}>
     <BrowserRouter>
