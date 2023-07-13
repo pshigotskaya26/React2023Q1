eeb65f5b053485015a6b70b7b7e3b87367c8ed38
pshigotskaya26 <pshihotskaya.tanya@gmail.com>
2023-07-13 07:46:05 +0300

    feat: add text intoabout page

--- react-components/src/pages/about/about.tsx
+++ react-components/src/pages/about/about.tsx
@@ -8,6 +8,22 @@ const About = () => {
       <Header />
       <div className="container">
         <h1 className="about__title">About us</h1>
+        <p>
+          React-redux is a simple study project SPA that is designed to learn React, Redux Toolkit,
+          React Hook Form and use API and get data after sending request to API.
+        </p>
+        <h2 className="about__subtitle">Done:</h2>
+        <ul className="about__list">
+          <li>
+            getting API data after sending the request to API with the searching parameters and
+            getting the list of results is updating
+          </li>
+          <li>validation the form using React Hook Form</li>
+          <li>
+            saving all information when navigate between routes (Search, About and Form) using Redux
+            Toolkit
+          </li>
+        </ul>
       </div>
     </section>
   );
